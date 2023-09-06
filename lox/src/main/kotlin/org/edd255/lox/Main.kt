1c2cd77ec6bb9df3ef4d91e398a007a5c05fed6e
edd255 <abaddon@disroot.org>
2023-09-06 16:38:14 +0200

    feat(exprs): implemented expressions

--- lox/src/main/kotlin/org/edd255/lox/Main.kt
+++ lox/src/main/kotlin/org/edd255/lox/Main.kt
@@ -1,9 +1,36 @@
 package org.edd255.lox
 
+import org.edd255.lox.expr.Binary
+import org.edd255.lox.expr.AstPrinter
+import org.edd255.lox.expr.Unary
+import org.edd255.lox.expr.Literal
+import org.edd255.lox.expr.Grouping
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val lox = Lox()
+
+    val expr = Binary(
+        Unary(
+            Token(
+                TokenType.MINUS,
+                "-",
+                null,
+                1
+            ),
+            Literal(123)
+        ),
+        Token(
+            TokenType.STAR,
+            "*",
+            null,
+            1
+        ),
+        Grouping(
+            Literal(45.67)
+        )
+    )
+    println(AstPrinter().print(expr))
     when (args.size) {
         0 -> lox.runPrompt()
         1 -> lox.runFile(args[0])
---
+++ lox/src/main/kotlin/org/edd255/lox/expr/AstPrinter.kt
@@ -0,0 +1,34 @@
+package org.edd255.lox.expr
+
+class AstPrinter : Visitor<String> {
+    fun print(expr: Expr) : String {
+        return expr.accept(this)
+    }
+
+    override fun visitBinaryExpr(expr: Binary): String {
+        return parenthesize(expr.op.lexeme, expr.left, expr.right)
+    }
+
+    override fun visitGroupingExpr(expr: Grouping): String {
+        return parenthesize("group", expr.expr)
+    }
+
+    override fun visitLiteralExpr(expr: Literal): String {
+        return expr.value.toString()
+    }
+
+    override fun visitUnaryExpr(expr: Unary): String {
+        return parenthesize(expr.op.lexeme, expr.right)
+    }
+
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        val builder = StringBuilder()
+        builder.append("(").append(name)
+        for (expr in exprs) {
+            builder.append(" ")
+            builder.append(expr.accept(this))
+        }
+        builder.append(")")
+        return builder.toString()
+    }
+}
